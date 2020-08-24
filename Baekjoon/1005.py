6c47e3e482c1f2f622da27fa3a089bdb1f59fb75
Rua-427 <70061403+Rua-427@users.noreply.github.com>
2020-08-24 23:04:00 +0900

    Update 1005.py

--- Baekjoon/1005.py
+++ Baekjoon/1005.py
@@ -1,6 +1,3 @@
-# your code goes here
-
-
 T = int(input())
 for i in range(T):
 	N, K = map(int, input().split())
@@ -15,36 +12,48 @@ for i in range(T):
 		
 		build[X-1][Y-1] = True
 		table[Y-1] += 1
-	
+
 	W = int(input())
-	time = 0
+	t = 0
 	queue = []
 	order = []
-	
-	end = False
-	for ii in range(N):
-		if(table[ii] == 0)
-			queue.append(ii)
-			if(ii + 1 == W):
-				time += W
+	end = False 
+	while(not end):
+    	# Push in Queue
+		for ii in range(N):
+			if(table[ii] == 0):
+				queue.append(ii)
+				table[ii] = -1
+				
+				
+		
+		m = 0
+		for o in queue:
+			
+			if(m < D[o]):
+				m = D[o]
+
+			if(o + 1 == W):
+				t += D[o]
+				
 				end = True
 				break
+		
+		if(end):
+			break
+		
+		t += m
+		if(not end):
+			for q in queue:
+				order.append(q)
+				for ii in range(N):
+					if(build[q][ii] == True):
+						build[q][ii] = False
+						table[ii] -= 1
 			
-	if(not end):
-		for q in queue:
-			order.append(q)
-			for ii in range(N):
-				if(build[q][ii] == True)
-					build[q][ii] = False
-					table[ii] -= 1
-		
-		m = 0		
-		for o in order:
-			if(m < D[o])
-				m = D[o]
-		
-		time += m
-		
-	
-	
+			queue = []
 			
+			
+						
+						
+	print(t)
