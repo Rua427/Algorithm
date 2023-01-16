0d13442e34f855ad7983cd1ac623f5d2e69cab36
Rua427 <ekrua427@naver.com>
2023-01-16 21:46:23 +0900

    백준 24060 병합 정렬

--- Baekjoon/Sorting/24060.js
+++ Baekjoon/Sorting/24060.js
@@ -1,5 +1,61 @@
 let fs = require("fs");
-let input = fs.readFileSync("/Users/rua/Documents/javascript/input.txt").toString().split("\n");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/Users/rua/Documents/javascript/input.txt";
 
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map((v) => + v));
 
-//합병정렬, 분할 정복
+const [[N, K], arr] = input;
+
+// N = arr.length
+
+// K -> 배열 merge과정에서 K번째 저장되는 수를 구해야 함.
+
+const merge = (arr1, arr2) => {
+  let result = [];
+  let [i, j] = [0, 0];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+    count++;
+    if (count === K) target = result[result.length - 1];
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+    count++;
+    if (count === K) target = result[result.length - 1];
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+    count++;
+    if (count === K) target = result[result.length - 1];
+  }
+  return result;
+};
+
+let count = 0;
+let target;
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.ceil(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
+
+mergeSort(arr);
+if (!target) target = -1;
+console.log(target);
