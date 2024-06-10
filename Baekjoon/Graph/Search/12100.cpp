d6149f31440bb5dbe3be2a609324b9f9f3399c1c
Rua <70061403+Rua427@users.noreply.github.com>
2024-06-10 15:46:41 +0900

    Update 12100.cpp

--- Baekjoon/Graph/Search/12100.cpp
+++ Baekjoon/Graph/Search/12100.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <stack>
-#include <queue>
 #include <list>
+
 using namespace std;
 
 int N;
+int maxResult = 0;
 
 struct Map {
-	int data[21][21]= {{0,} };
+	int data[21][21] = { {0,} };
+public:
+	bool operator==(const Map& map) const {
+		for (int y = 0; y < 21; y++)
+		{
+			for (int x = 0; x < 21; x++)
+			{
+				if (data[y][x] != map.data[y][x]) return false;
+			}
+		}
 
+		return true;
+	}
 };
 struct Board {
 	Map map;
@@ -20,150 +32,13 @@ struct Board {
 	}
 };
 
-void Move(Map m, int i) {
-	list<int> _list;
-	list<int> value;
-	//left
-	if (i == 0) {
-		for (int y = 1; y <= N; y++)
-		{
-			for (int x = 1; x <= N; x++)
-			{
-				if (m.data[y][x] != 0) {
+int DFS(Board board);
+void Move(Map& m, int i);
+list<int> GetCalcList(list<int> _list);
+void InsertCalcList(Map& m, list<int>& _list, int x, int y);
 
-					if (!_list.empty() && _list.back() == m.data[y][x]) {
-						value.push_back(_list.back() + m.data[y][x]);
-						_list.pop_front();
-						continue;
-					}
-					_list.push_back(m.data[y][x]);
-				}
-			}
-
-			value.splice(value.end(), _list);
-
-			for (int x = 1; x <= N; x++)
-			{
-				if (value.empty()) m.data[y][x] = 0;
-				else {
-					m.data[y][x] = value.front();
-					value.pop_front();
-				}
-			}
-		}
-	}
-	//right
-	else if (i == 1) {
-		for (int y = 1; y <= N; y++)
-		{
-			for (int x = N; x > 0; x--)
-			{
-				// 인접한 값
-				if (m.data[y][x] != 0) {
-					if (!_list.empty() && _list.back() == m.data[y][x]) {
-						value.push_back(_list.back() + m.data[y][x]);
-						_list.pop_front();
-						continue;
-					}
-					_list.push_back(m.data[y][x]);
-				}
-			}
-
-			for (int x = N; x > 0; x--)
-			{
-				if (value.empty()) m.data[y][x] = 0;
-				else {
-					m.data[y][x] = value.front();
-					value.pop_front();
-				}
-			}
-		}
-	}
-	// up
-	else if (i == 2) {
-		for (int x = 1; x <= N; x++)
-		{
-			for (int y = 1; y <= N; y++)
-			{
-				// 인접한 값
-				if (m.data[y][x] != 0) {
-					if (!_list.empty() && _list.back() == m.data[y][x]) {
-						value.push_back(_list.back() + m.data[y][x]);
-						_list.pop_front();
-						continue;
-					}
-
-					_list.push_back(m.data[y][x]);
-				}
-			}
-
-			for (int y = 1; y <= N; y++)
-			{
-				if (value.empty()) m.data[y][x] = 0;
-				else {
-					m.data[y][x] = value.front();
-					value.pop_front();
-				}
-			}
-		}
-	}
-	//down
-	else if (i == 3) {
-		for (int x = 1; x <= N; x++)
-		{
-			for (int y = N; y > 0; y--)
-			{
-				// 인접한 값
-				if (m.data[y][x] != 0) {
-					if (!_list.empty() && _list.back() == m.data[y][x]) {
-						value.push_back(_list.back() + m.data[y][x]);
-						_list.pop_front();
-						continue;
-					}
-
-					_list.push_back(m.data[y][x]);
-				}
-			}
-
-			for (int y = N; y > 0; y--)
-			{
-				if (value.empty()) m.data[y][x] = 0;
-				else {
-					m.data[y][x] = value.front();
-					value.pop_front();
-				}
-			}
-		}
-	}
-}
-
-void DFS(Board board) {
-
-	stack<Board> s;
-	s.push(board);
-
-	while (!s.empty()) {
-
-		Board current = s.top();
-		int count = s.top().count;
-		s.pop();
-
-		if (count >= 5) break;
-
-		for (int i = 0; i < 4; i++)
-		{
-			Map m = current.map;
-			int ncount = count + 1;
-			Move(m, i);
-
-			Board next = Board(m, ncount);
-			s.push(next);
-		}
-	}
-}
 
 int main() {
-
 	cin >> N;
 	Map getMap;
 
@@ -175,8 +50,149 @@ int main() {
 		}
 	}
 
-	DFS(Board{getMap, 0 });
-	//int result = DFS(Board{map, 0});
-	//cout << result;
+
+	int result = DFS(Board{ getMap, 0 });
+	cout << result;
+
 	return 0;
+}
+
+int DFS(Board board)
+{
+
+	stack<Board> s;
+	s.push(board);
+	while (!s.empty())
+	{
+		Board current = s.top();
+		int count = s.top().count;
+		s.pop();
+
+		if (count >= 5) continue;
+
+		for (int i = 0; i < 4; i++)
+		{
+			Map nm = current.map;
+			int ncount = count + 1;
+			Move(nm, i);
+
+			if (current.map == nm) continue;
+
+			Board next = Board(nm, ncount);
+			s.push(next);
+		}
+	}
+
+	return maxResult;
+}
+
+void Move(Map& m, int i)
+{
+	list<int> _list;
+	list<int> value;
+	//left
+	if (i == 0) {
+		for (int y = 1; y <= N; y++)
+		{
+			for (int x = 1; x <= N; x++)
+			{
+				if (m.data[y][x] == 0) continue;
+				value.push_back(m.data[y][x]);
+			}
+
+			_list = GetCalcList(value);
+
+			for (int x = 1; x <= N; x++)
+			{
+				InsertCalcList(m, _list, x, y);
+			}
+
+			value.clear();
+		}
+	}
+	//right
+	else if (i == 1) {
+		for (int y = 1; y <= N; y++)
+		{
+			for (int x = N; x > 0; x--)
+			{
+				if (m.data[y][x] == 0) continue;
+				value.push_back(m.data[y][x]);
+			}
+
+			_list = GetCalcList(value);
+
+			for (int x = N; x > 0; x--)
+			{
+				InsertCalcList(m, _list, x, y);
+			}
+			value.clear();
+		}
+	}
+	// up
+	else if (i == 2) {
+		for (int x = 1; x <= N; x++)
+		{
+			for (int y = 1; y <= N; y++)
+			{
+				if (m.data[y][x] == 0) continue;
+				value.push_back(m.data[y][x]);
+			}
+
+			_list = GetCalcList(value);
+			for (int y = 1; y <= N; y++)
+			{
+				InsertCalcList(m, _list, x, y);
+			}
+			value.clear();
+		}
+	}
+	//down
+	else if (i == 3) {
+		for (int x = 1; x <= N; x++)
+		{
+			for (int y = N; y > 0; y--)
+			{
+				if (m.data[y][x] == 0) continue;
+				value.push_back(m.data[y][x]);
+			}
+
+			_list = GetCalcList(value);
+
+			for (int y = N; y > 0; y--)
+			{
+				InsertCalcList(m, _list, x, y);
+			}
+			value.clear();
+		}
+	}
+}
+
+list<int> GetCalcList(list<int> v)
+{
+	list<int> _list;
+	int value = 0;
+	for (list<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		if (value == *it) {
+			_list.pop_back();
+			_list.push_back(*it * 2);
+			value = 0;
+			continue;
+		}
+		value = *it;
+		_list.push_back(value);
+	}
+
+	return _list;
+}
+
+void InsertCalcList(Map& m, list<int>& _list, int x, int y)
+{
+	if (_list.empty()) m.data[y][x] = 0;
+	else {
+		m.data[y][x] = _list.front();
+		if (m.data[y][x] > maxResult) maxResult = m.data[y][x];
+		_list.pop_front();
+	}
 }
