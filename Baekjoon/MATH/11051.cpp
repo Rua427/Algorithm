a048f64bea7159e8a8fcd2347249c63dcf9e7c1e
Rua427 <ekrua427@naver.com>
2024-08-12 00:34:28 +0900

    11051 이항계수 2

---
+++ Baekjoon/MATH/11051.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#define P 10007
+using namespace std;
+
+long long N, K;
+
+long long Power(long long a, long long n)
+{
+    long long result = 1;
+
+    while (n > 0)
+    {
+        if (n & 1)
+        {
+            result = result * a % P;
+        }
+        a = a * a % P;
+        n >>= 1;
+    }
+    return result;
+}
+long long Factorial(long long start, long long end)
+{
+    long long result = 1;
+
+    for (long long i = start; i <= end; i++)
+    {
+        result = result * i % P;
+    }
+
+    return result;
+}
+
+int main()
+{
+    cin >> N >> K;
+
+    long long A = Factorial(1, N) % P;
+    long long B = Factorial(1, N - K) * Factorial(1, K) % P;
+    long long result = A * Power(B, P - 2) % P;
+    cout << result;
+
+    return 0;
+}
